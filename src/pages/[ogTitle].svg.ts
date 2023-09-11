9b66072d449dcbe039ffe365f522d927c6727621
Stokey <stokeyx@protonmail.com>
2023-09-10 22:44:04 -0300

    fix: temporary fix og image generator

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -13,6 +13,8 @@ export function getStaticPaths() {
   return posts
     .filter(({ data }) => !data.ogImage)
     .map(({ data }) => ({
-      params: { ogTitle: data.title },
+      params: {
+        ogTitle: data.title.replace(/[^a-zA-Z0-9]/g, "-"),
+      },
     }));
 }
